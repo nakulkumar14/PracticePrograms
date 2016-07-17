4f5a0a8773a3629d699252235e9795f7e9856456
Nakul Kumar <nakul.k@bankbazaar.com>
2016-07-17 16:58:21 +0530

    Linked list-reverse and delete

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -40,6 +40,59 @@ public class LinkedList {
         }
     }
 
+    void insertAtLocation(int data, int position) {
+        if (head == null) {
+            head = new Node(data);
+        } else {
+            if (position == 0)
+                insertAtHead(data);
+            else {
+                Node temp = head;
+                int locationBeforeIndex = 0;
+                while (locationBeforeIndex != position - 1) {
+                    temp = temp.next;
+                    locationBeforeIndex++;
+                }
+
+                Node newnode = new Node(data);
+                newnode.next = temp.next;
+                temp.next = newnode;
+            }
+        }
+    }
+
+    void deleteFromHead() {
+        if (head == null)
+            return;
+        head = head.next;
+    }
+
+    void deleteFromTail() {
+        if (head == null)
+            return;
+        if (head.next == null) {
+            head = null;
+        } else {
+            Node temp = head;
+            while (temp.next.next != null)
+                temp = temp.next;
+
+            temp.next = null;
+        }
+    }
+
+    void reverse() {
+        Node a = head;
+        Node b, c = null;
+        while (a != null) {
+            b = a;
+            a = a.next;
+            b.next = c;
+            c = b;
+        }
+        head = c;
+    }
+
     boolean isEmpty() {
         return head == null ? true : false;
     }
@@ -63,6 +116,18 @@ public class LinkedList {
         list1.insertAtHead(5);
         list1.show();
         list1.insertAtTail(10);
+        list1.show();
+        list1.insertAtLocation(7, 0);
+        list1.show();
+        list1.insertAtLocation(8, 2);
+        list1.show();
+
+        list1.reverse();
+        list1.show();
+
+        list1.deleteFromHead();
+        list1.show();
+        list1.deleteFromTail();
         list1.show();
     }
 }
