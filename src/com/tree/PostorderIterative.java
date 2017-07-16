9eb6449a7cacbad61167f9eaf4783ce59fbfef6c
nakulkumar <nakulkumar14.09@gmail.com>
2017-07-16 13:09:42 +0530

    Post order traversal using 1 stack

--- src/com/tree/PostorderIterative.java
+++ src/com/tree/PostorderIterative.java
@@ -7,6 +7,7 @@ import java.util.Stack;
  */
 public class PostorderIterative {
 
+    // using 2 stacks
     static void postorderIterative(TNode root){
         if (root == null)
             return;
@@ -31,6 +32,41 @@ public class PostorderIterative {
         }
     }
 
+    static void postOrderSingleStack(TNode root){
+        if (root == null)
+            return;
+
+        Stack<TNode> s = new Stack<>();
+
+        while (!s.isEmpty() || root != null)
+        {
+            // push right then root, go to left as much as possible
+            while (root != null)
+            {
+                if (root.right != null)
+                    s.add(root.right);
+                s.add(root);
+                root = root.left;
+            }
+
+            root = s.pop();
+
+            // use root's right and repeat above process
+            if (root.right != null && !s.isEmpty()
+                    && root.right == s.peek())
+            {
+                TNode rootRight = s.pop();
+                s.push(root);
+                root = rootRight;
+            } else
+            {
+                System.out.print(root.data + " ");
+                root = null;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         TNode root = null;
         root = TNode.insertToBST(root,4);
@@ -38,9 +74,26 @@ public class PostorderIterative {
         root = TNode.insertToBST(root,2);
         root = TNode.insertToBST(root,3);
 
+        System.out.println("Inorder : ");
         TNode.inorder(root);
 
-        System.out.println("\nPostorder : ");
+        System.out.println("\nPostorder - 2 ss : ");
         postorderIterative(root);
+
+        root = null;
+        root = TNode.insertToBST(root,5);
+        root = TNode.insertToBST(root,3);
+        root = TNode.insertToBST(root,7);
+        root = TNode.insertToBST(root,2);
+        root = TNode.insertToBST(root,4);
+        root = TNode.insertToBST(root,6);
+        root = TNode.insertToBST(root,8);
+
+
+        System.out.println("\nInorder : ");
+        TNode.inorder(root);
+
+        System.out.println("\nPostorder - 1 s : ");
+        postOrderSingleStack(root);
     }
 }
