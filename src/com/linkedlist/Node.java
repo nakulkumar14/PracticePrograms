f5cfe99fec0bf00503db3a1878ec5a3b5a55f0ae
nakulkumar <nakulkumar14.09@gmail.com>
2017-07-23 15:51:58 +0530

    Moved Node class out

---
+++ src/com/linkedlist/Node.java
@@ -0,0 +1,14 @@
+package com.linkedlist;
+
+/**
+ * Created by nakul on 23/7/17.
+ */
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
