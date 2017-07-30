4cfcb75cef64af8a57740e3ac5b00c03cb140580
nakulkumar <nakulkumar14.09@gmail.com>
2017-07-30 21:51:31 +0530

    LRUCache Implementation

--- src/com/practice/LRUCache.java
+++ src/com/practice/LRUCache.java
@@ -8,6 +8,15 @@ import java.util.Map;
  */
 // LRU Cache implementation
 public class LRUCache {
+
+    Queue queue;            // contain pages in which they are accessed
+    Map<Integer,Node> map;  // contain map of pages with their addresses in queue.
+
+    public LRUCache(int capacity) {
+        queue = new Queue(null,null,0,capacity);
+        map = new HashMap<>();
+    }
+
     class Node{
         int page;
         Node prev,next;
@@ -18,32 +27,111 @@ public class LRUCache {
         }
     }
 
-    static class Dequeue{
-        int pno;
-        int filled;
-        Node front,rear;
-        Dequeue(int pno){
-            this.pno = pno;
-            this.filled = 0;
-            this.front = this.rear = null;
+    class Queue{
+        Node head,tail;
+        int size,capacity;
+
+        public Queue(Node head, Node tail, int size, int capacity) {
+            this.head = head;
+            this.tail = tail;
+            this.size = size;
+            this.capacity = capacity;
         }
     }
 
-    Map<Integer,Node> map = new HashMap<Integer,Node>();
+    // Add node to the head of the list.
+    Node addToList(int data){
+        // if queue is empty make head and tail same.
+        if (queue.head == null){
+            queue.head = new Node(data);
+            queue.tail = queue.head;
+            queue.size = 1;
 
-    static void display(Dequeue d){
-        Dequeue temp = d;
-        int pageno=0;
-        while (pageno!=temp.pno){
-            System.out.print(temp.front.page+" ");
-            pageno++;
-            temp.front = temp.front.next;
+            return queue.head;
         }
-        System.out.println();
+        else if (queue.size < queue.capacity)   // increase the size of queue.
+            queue.size++;
+        else{
+            // if queue is full, remove the tail.
+            queue.tail = queue.tail.prev;
+            queue.tail.next = null;
+        }
+
+        // create the new node and add to head of queue and set the references properly.
+        Node newHead = new Node(data);
+        newHead.next = queue.head;
+        queue.head.prev = newHead;
+        queue.head = newHead;
+
+        return queue.head;
+    }
+
+    void moveToHead(Node page){
+
+        // if node to be moved is at head then return.
+        if (queue.head == page)
+            return;
+
+        // if node to be moved to head is at tail remove the tail.
+        if (queue.tail == page){
+            queue.tail = queue.tail.prev;
+            queue.tail.next = null;
+        }
+
+        // adjust the next and prev pointers of node to be moved.
+        Node prev = page.prev;
+        Node next = page.next;
+        prev.next = next;
+        if (next!=null)
+            next.prev = prev;
+
+        // make node as head.
+        page.next = queue.head;
+        page.prev = null;
+        queue.head.prev = page;
+
+        queue.head = page;
+    }
+
+    // print contents of queue.
+    void show(){
+        Node temp = queue.head;
+        while (temp!=queue.tail){
+            System.out.print(temp.page + " ");
+            temp = temp.next;
+        }
+        System.out.println(queue.tail.page);
+    }
+
+    void accessPage(int page){
+
+        // check if page to be accessed is in map
+        // if so move it to head of queue.
+        if (map.get(page)!=null){
+            Node node = map.get(page);
+            moveToHead(node);
+        }else{
+
+            // if queue is full, remove tail
+            if (queue.size == queue.capacity)
+                map.remove(queue.tail.page);
+
+            // add node to queue and put in map.
+            Node node = addToList(page);
+            map.put(page, node);
+        }
     }
 
     public static void main(String[] args) {
-        Dequeue d = new Dequeue(5);
-        display(d);
+        LRUCache lruCache = new LRUCache(4);
+        lruCache.accessPage(1);
+        lruCache.accessPage(2);
+        lruCache.accessPage(3);
+        lruCache.accessPage(1);
+        lruCache.accessPage(4);
+        lruCache.accessPage(5);
+
+        lruCache.show();
+
     }
 }
