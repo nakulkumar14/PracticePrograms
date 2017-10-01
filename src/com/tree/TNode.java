375840d7fae9ea67187e88d90e9290aa26e753da
Nakul Kumar <nakulkumar14.09@gmail.com>
2017-10-01 14:43:19 +0530

    Added nextRight in TNode

--- src/com/tree/TNode.java
+++ src/com/tree/TNode.java
@@ -6,12 +6,15 @@ package com.tree;
 public class TNode {
     int data;
     TNode left,right;
+    TNode nextRight;
 
     public TNode() {
     }
 
     public TNode(int data) {
         this.data = data;
+        this.left = null;
+        this.right = null;
     }
 
     public static TNode createNode(int data){
@@ -34,6 +37,14 @@ public class TNode {
             inorder(root.left);
             System.out.print(root.data+" ");
             inorder(root.right);
+        }
+    }
+
+    public static void preorder(TNode root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
         }
     }
 }
