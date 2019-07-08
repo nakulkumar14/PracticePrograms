c3074bcf1f2d86b14e59e39c32d0c82fd7a022d6
Nakul Kumar <nakul.kumar@lenskart.in>
2019-07-09 00:02:58 +0530

    Nuts and bolts problem

---
+++ src/com/array/NutsAndBolts.java
@@ -0,0 +1,34 @@
+package com.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NutsAndBolts {
+
+    static void sort(char nuts[], char bolts[]) {
+
+        System.out.println(Arrays.toString(nuts));
+        System.out.println(Arrays.toString(bolts));
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < nuts.length; i++) {
+            map.put(nuts[i], i);
+        }
+
+        for (int i = 0; i < bolts.length; i++) {
+            if (map.containsKey(bolts[i])) {
+                nuts[i] = bolts[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(nuts));
+        System.out.println(Arrays.toString(bolts));
+    }
+
+    public static void main(String[] args) {
+        char nuts[] = {'@', '#', '$', '%', '^', '&'};
+        char bolts[] = {'$', '%', '&', '^', '@', '#'};
+        sort(nuts, bolts);
+    }
+}
