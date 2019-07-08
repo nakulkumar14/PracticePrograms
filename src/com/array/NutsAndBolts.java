3d45e8946bdad662736fc8eee91087d66dd592f9
Nakul Kumar <nakul.kumar@lenskart.in>
2019-07-09 00:28:24 +0530

    Nuts and bolts problem 2

--- src/com/array/NutsAndBolts.java
+++ src/com/array/NutsAndBolts.java
@@ -7,6 +7,41 @@ import java.util.Map;
 // https://www.geeksforgeeks.org/nuts-bolts-problem-lock-key-problem-set-2-hashmap/
 public class NutsAndBolts {
 
+    static void matchPairs(char nuts[], char bolts[], int l, int h) {
+        if (l < h) {
+            int pivot = partition(nuts, l, h, bolts[h]);
+
+            partition(bolts, l, h, nuts[pivot]);
+
+            matchPairs(nuts, bolts, l, pivot - 1);
+            matchPairs(nuts, bolts, pivot + 1, h);
+        }
+    }
+
+    static int partition(char[] a, int l, int h, char pivot) {
+        int i = l;
+        for (int j = l; j < h; j++) {
+            if (a[j] < pivot) {
+                char temp = a[i];
+                a[j] = a[i];
+                a[i] = temp;
+                i++;
+            } else if (a[j] == pivot) {
+                char temp = a[h];
+                a[h] = a[j];
+                a[j] = temp;
+                j--;
+            }
+        }
+
+        char temp = a[i];
+        a[i] = a[h];
+        a[h] = temp;
+
+        return i;
+    }
+
+
     static void sort(char nuts[], char bolts[]) {
 
         System.out.println(Arrays.toString(nuts));
@@ -30,6 +65,10 @@ public class NutsAndBolts {
     public static void main(String[] args) {
         char nuts[] = {'@', '#', '$', '%', '^', '&'};
         char bolts[] = {'$', '%', '&', '^', '@', '#'};
-        sort(nuts, bolts);
+//        sort(nuts, bolts);
+        matchPairs(nuts, bolts, 0, nuts.length - 1);
+
+        System.out.println(Arrays.toString(nuts));
+        System.out.println(Arrays.toString(bolts));
     }
 }
